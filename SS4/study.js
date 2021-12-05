47b17833c7dc6cc2091409846653a4523143540b
Alter <92667171+alterguda9789@users.noreply.github.com>
2021-12-05 15:49:07 +0700

    ok

---
+++ SS4/study.js
@@ -0,0 +1,19 @@
+let a = "hi"
+console.log(a);
+
+
+var b = "blcok b"
+console.log(b);
+
+
+const Pi = 3.14;
+console.log(3*Pi);
+
+{
+    {
+        let vong2 = "vong 2"
+        {
+            console.log(vong2);
+        }
+    }
+}
