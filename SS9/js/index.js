e85d17f1e7466f2e1604c1523b4cc7787a6fef61
Alter <92667171+alterguda9789@users.noreply.github.com>
2021-12-19 17:04:35 +0700

    Register/login's done

---
+++ SS9/js/index.js
@@ -0,0 +1,21 @@
+// const init = () => {
+//   document.getElementById('app')
+//     .innerHTML = components.register
+
+
+//   document.getElementById("redirect-to-login")
+//     .addEventListener('click', () => {
+//       document.getElementById('app')
+//         .innerHTML = components.login
+//     })
+
+
+//   document.getElementById("redirect-to-register")
+//     .addEventListener('click', () => {
+//       document.getElementById('app')
+//         .innerHTML = components.register
+//     })
+
+// }
+
+// window.onload = init;
